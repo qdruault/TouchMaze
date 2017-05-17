229801a2ba213061107f825f1bcf40b55b7b04f6
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2017-05-17 19:32:44 +0200

    [Bluetooth] Commentaires

--- Bluetooth/app/src/main/java/com/example/labocred/bluetooth/BluetoothReceiver.java
+++ Bluetooth/app/src/main/java/com/example/labocred/bluetooth/BluetoothReceiver.java
@@ -11,13 +11,24 @@ import static com.example.labocred.bluetooth.BluetoothSettingsActivity.app;
  * Created by LaboCred on 03/05/2017.
  */
 
+/**
+ * Classe permettant "d'écouter" les messages qu'on lui envoit.
+ */
 public class BluetoothReceiver extends BroadcastReceiver{
+    /**
+     * A la réception d'un message.
+     * @param context
+     * @param intent
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Si l'action est StreamBluetoth.
         if(intent.getAction().equals("com.example.labocred.bluetooth.StreamBluetooth")){
             try {
+                // On extrait le tableau de bytes passé en extra.
                 Bundle extras = intent.getExtras();
                 byte[] bytes = extras.getByteArray("BStream");
+                // On se connecte à la socket du bluetooth et on lui envoie le message.
                 new BluetoothReceiverSettings.ConnectedThread(app.socket).write(bytes);
             } catch (NullPointerException e) {
                 e.printStackTrace();
--- Bluetooth/app/src/main/java/com/example/labocred/bluetooth/BluetoothReceiverSettings.java
+++ Bluetooth/app/src/main/java/com/example/labocred/bluetooth/BluetoothReceiverSettings.java
@@ -14,6 +14,7 @@ import java.io.OutputStream;
 
 public class BluetoothReceiverSettings extends Activity{
     private DialogApp app;
+    // La connexion au Bluetooth.
     public static ConnectedThread bluetoothThread;
 
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +22,7 @@ public class BluetoothReceiverSettings extends Activity{
         super.onCreate(savedInstanceState);
         app = (DialogApp) getApplication();
         try {
+            // On crée la connexion en bluetooth.
             bluetoothThread = new ConnectedThread(app.socket);
             bluetoothThread.run();
         }catch(NullPointerException e){
@@ -28,39 +30,59 @@ public class BluetoothReceiverSettings extends Activity{
         }
     }
 
+    /**
+     * Thread gérant la connexion au Bluetooth.
+     */
     public static class ConnectedThread extends Thread {
+        // La socket.
         private final BluetoothSocket mmSocket;
+        // Flux d'entrée de la socket.
         private final InputStream mmInStream;
+        // Flux de sortie de la socket.
         public final OutputStream mmOutStream;
 
+        /**
+         * Constructeur
+         * @param socket : la socket à utiliser.
+         */
         public ConnectedThread(BluetoothSocket socket) {
+            // On initialise la socket.
             mmSocket = socket;
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
 
             // Get the input and output streams, using temp objects because member streams are final
             try {
+                // On récupère les flux d'entrée et de sortie de la socket.
                 tmpIn = socket.getInputStream();
                 tmpOut = socket.getOutputStream();
             } catch (IOException e) { }
 
+            // On initialise les flux d'E/S.
             mmInStream = tmpIn;
             mmOutStream = tmpOut;
         }
 
         public void run() {}
 
-        /* Call this from the main activity to send data to the remote device */
+        /**
+         * Permet de lever les picots.
+         * @param bytes : un tableau représentant les picots à lever.
+         */
         public void write(byte[] bytes) {
             try {
+                // Envoie un flux de données pour lever les picots.
                 mmOutStream.write(bytes);
                 mmOutStream.flush();
             } catch (IOException e) { }
         }
 
-        /* Call this from the main activity to shutdown the connection */
+        /**
+         * Ferme la connexion bluetooth.
+         */
         public void cancel() {
             try {
+                // Ferme la socket.
                 mmSocket.close();
             } catch (IOException e) { }
         }
