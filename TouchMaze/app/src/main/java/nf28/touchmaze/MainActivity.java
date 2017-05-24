c92deeb4f93e8a9ff2d9bcf947bd729830a4d2e6
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2017-05-24 15:38:04 +0200

    [Connexion] Binding des champs + listener bouton

--- TouchMaze/app/src/main/java/nf28/touchmaze/MainActivity.java
+++ TouchMaze/app/src/main/java/nf28/touchmaze/MainActivity.java
@@ -1,13 +1,67 @@
 package nf28.touchmaze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginText;
+    EditText passwordText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Binding.
+        loginText = (EditText)findViewById(R.id.text_login);
+        passwordText = (EditText)findViewById(R.id.text_password);
+        loginButton = (Button)findViewById(R.id.btn_login);
+
+        // Listener.
+        loginButton.setOnClickListener(addListenerLoginButton());
+    }
+
+    /**
+     * Création du listener du bouton de connexion.
+     * @return
+     */
+    private OnClickListener addListenerLoginButton() {
+        OnClickListener listener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // On vérifie que les champs sont remplis.
+                if (loginText.getText().toString().isEmpty() || passwordText.getText().toString().isEmpty()) {
+                    onLoginFailed();
+                } else {
+                    onLoginSuccess();
+                }
+            }
+        };
+
+        return listener;
+    }
+
+    /**
+     * Connexion réussie.
+     */
+    private void onLoginSuccess() {
+        Toast.makeText(getBaseContext(), "Connexion réussie ! :D", Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Echec de connexion.
+     */
+    private void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login ou mot de passe incorrect :(", Toast.LENGTH_LONG).show();
     }
 }
